3cc538ba9f88591f29304623c0e330bc7eb22bd6
surajpiyush <surajpiyush9648@gmail.com>
2023-07-30 20:25:06 +0530

    add some package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
-
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Home from "./Home";
-
 import Popular from "./Popular";
 import TopRated from "./TopRated";
 import UpComing from "./UpComing";
-
-
 import Details from "./Details";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="popular" element={<Popular />}></Route>
-        
+        <Route path="popular" element={<Popular />}></Route>        
         <Route path="upcoming" element={<UpComing />}></Route>
         <Route path="details/:id" element={<Details/> }></Route>
         <Route path="rated" element={<TopRated />}></Route>
